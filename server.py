3a966263b99ba4f9d7bc35eeee27bdc313b08ff3
Bulat Karmak <wkwdim@gmail.com>
2021-03-10 22:32:20 +0300

    Прошёл трек Flask - неделя 4

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
+from python_org_news import get_python_news
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = 'Новости Python'
     weather = weather_by_city('Kazan, Russia')
-    if weather:
-        return f'Температура в Казани: {weather["temp_C"]} °C (ощущается как {weather["FeelsLikeC"]})'
-    else:
-        return f'Сервис погоды временно недоступен'
+    news_list = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
