2722732d76528f226859d00974234073f279ea94
Bulat Karmak <wkwdim@gmail.com>
2021-03-21 11:31:45 +0300

    Удалил БД

--- webapp/db1/get_user.py
+++
@@ -1,7 +0,0 @@
-from models import User
-
-first_user = User.query.first()
-print(f'''Имя Фамилия: {first_user.name}
-Зарплата: {first_user.salary} долларов
-Email: {first_user.email}
-''')
--- webapp/db1/update_user.py
+++
@@ -1,6 +0,0 @@
-from db import db_session
-from models import User
-
-user = User.query.first()
-user.salary += 200
-db_session.commit()
